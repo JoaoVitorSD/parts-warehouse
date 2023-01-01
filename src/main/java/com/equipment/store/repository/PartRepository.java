642c944e42eca62554e0d5585a7f0fc1da95cd81
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-01-01 14:52:17 -0300

    feat: JpaRepository para a classe Part

---
+++ src/main/java/com/equipment/store/repository/PartRepository.java
@@ -0,0 +1,7 @@
+package com.equipment.store.repository;
+
+import com.equipment.store.entities.Part;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartRepository extends JpaRepository<Part,String> {
+}
