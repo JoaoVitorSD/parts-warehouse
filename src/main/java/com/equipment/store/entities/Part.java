c0012baae4313a0c88e8c955e69451347b74e5bf
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-01-01 14:42:53 -0300

    feat: setup do h2 db

--- src/main/java/com/equipment/store/entities/Part.java
+++ src/main/java/com/equipment/store/entities/Part.java
@@ -3,9 +3,13 @@ package com.equipment.store.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "warehouse_parts")
+@Getter
+@Setter
 public class Part {
     @Id
     private String id;
@@ -15,4 +19,15 @@ public class Part {
     private Integer numberLocal;
 
     private Integer quantity;
+
+    public Part() {};
+    public Part(String id, String description, Integer warehouse, Integer numberLocal, Integer quantity) {
+        this.id = id;
+        this.description = description;
+        this.warehouse = warehouse;
+        this.numberLocal = numberLocal;
+        this.quantity = quantity;
+    }
+
+
 }
